32df9c94126fb9decf6641ffdb76201156d2654a
digvijaypuri <41981248+digvijaypuri@users.noreply.github.com>
2024-12-21 20:54:00 +0530

    Update create-ec2-with-terraform.tf

--- create-ec2-with-terraform.tf
+++ create-ec2-with-terraform.tf
@@ -2,25 +2,22 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.0"
     }
   }
 }
-
 provider "aws" {
-  region     = "AWS_REGION"
-  access_key = "AWS_ACCESS_KEY"
-  secret_key = "AWS_SECRET_KEY"
+  region     = "us-east-1"
+  access_key = ""
+  secret_key = ""
 }
-
 // To Generate Private Key
 resource "tls_private_key" "rsa_4096" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
-
 variable "key_name" {
-  description = "Name of the SSH key pair"
+  
 }
 
 // Create Key Pair for Connecting EC2 via SSH
@@ -36,37 +33,19 @@ resource "local_file" "private_key" {
 }
 
 # Create a security group
-resource "aws_security_group" "sg_ec2" {
-  name        = "sg_ec2"
-  description = "Security group for EC2"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
 
 resource "aws_instance" "public_instance" {
-  ami                    = "ami-0f5ee92e2d63afc18"
+  ami                    = "ami-05b4ded3ceb71e470"
   instance_type          = "t2.micro"
   key_name               = aws_key_pair.key_pair.key_name
-  vpc_security_group_ids = [aws_security_group.sg_ec2.id]
+  // vpc_security_group_ids = [aws_security_group.sg_ec2.id]
 
   tags = {
     Name = "public_instance"
   }
 
-  root_block_device {
-    volume_size = 30
-    volume_type = "gp2"
-  }
+  //root_block_device {
+     //volume_size = 30
+    //volume_type = "gp2"
+ // }
 }
